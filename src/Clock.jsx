05b493a9c6ebeb8a5b03f76002f46276c693a14d
Deniss <den.b@itservicesgroup.co.uk>
2017-08-15 18:52:39 +0100

    Add .leading0, numbers in 2 digits format
    341 days 05 hours 05 minutes 03 seconds
    insted of
    341 days 5 hours 5 minutes 3 seconds

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -25,13 +25,16 @@ class Clock extends Component {
     const days =  Math.floor((time/(1000*60*60*24)))
     this.setState({days, hours, minutes, seconds})
   }
+  leading0(num) {
+    return num < 10 ? '0' + num : num
+  }
   render() {
     return(
       <div>
-        <div className="Clock-days"> {this.state.days} days </div>
-        <div className="Clock-hours">{this.state.hours} hours </div>
-        <div className="Clock-minutes"> {this.state.minutes} minutes </div>
-        <div className="Clock-seconds"> {this.state.seconds} seconds </div>
+        <div className="Clock-days"> {this.leading0(this.state.days)} days </div>
+        <div className="Clock-hours">{this.leading0(this.state.hours)} hours </div>
+        <div className="Clock-minutes"> {this.leading0(this.state.minutes)} minutes </div>
+        <div className="Clock-seconds"> {this.leading0(this.state.seconds)} seconds </div>
       </div>
     )
   }
